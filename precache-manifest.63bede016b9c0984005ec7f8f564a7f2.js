db9868097c27be7c8e12cc27adb9eba99f80df8b
George Thorp <gthorp@ims.tech>
2022-09-12 09:58:57 +0100

    Updates

---
+++ precache-manifest.63bede016b9c0984005ec7f8f564a7f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a312ee3a798a0a0d3d3",
+    "url": "/fpl-helper/static/css/main.37dc238b.chunk.css"
+  },
+  {
+    "revision": "7a312ee3a798a0a0d3d3",
+    "url": "/fpl-helper/static/js/main.7a312ee3.chunk.js"
+  },
+  {
+    "revision": "991710d60cc142e1f491",
+    "url": "/fpl-helper/static/js/1.991710d6.chunk.js"
+  },
+  {
+    "revision": "2b392852f840b56a81dc",
+    "url": "/fpl-helper/static/js/2.2b392852.chunk.js"
+  },
+  {
+    "revision": "334ef82c06f6efa669b4",
+    "url": "/fpl-helper/static/js/runtime~main.334ef82c.js"
+  },
+  {
+    "revision": "c902b939eddea448f0f775c7a442c9af",
+    "url": "/fpl-helper/index.html"
+  }
+];
